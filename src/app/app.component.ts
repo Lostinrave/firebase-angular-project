fa80f3a7ec5b8c5d1edd5b79fb96e5b1287c7bdb
Lostinrave <83598946+Lostinrave@users.noreply.github.com>
2021-11-25 18:38:23 +0200

    Data rendering inside template + dynamic progress bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,29 +12,38 @@ export class AppComponent {
   public title = 'quiz';
   public currentQuestion: number = 0;
   public progress: number = 0;
-  public answers : Array<any> = [];
-  public quizQuestions : Array<any> = [];
+  public answers: Array<any> = [];
+  public quizQuestions: Array<any> = [];
+  public quiz: any = {};
+
 
   constructor(db: AngularFireDatabase) {
-    db.list('quizes/abc').valueChanges()
-    .subscribe((data) =>{
-      this.answers = data;
-    })
+    db.object('quizes/abc').valueChanges().subscribe((data: any) => {
+      console.warn("Quiz data:");
+      this.quiz = data;
+      this.quizQuestions = data.questions;
+
+      console.warn(this.quizQuestions);
+    });
   }
 
-  nextQuestion(){
-    if(this.progress < 100){
+  nextQuestion() {
+    if (this.progress < 100) {
       this.currentQuestion++;
-      this.progress = this.currentQuestion / this.answers.length  * 100;
-      console.log("Progress: " + this.progress);
+      this.countProgress();
     }
   }
 
-  previousQuestion(){
-    if(this.progress > 0){
+  previousQuestion() {
+    if (this.progress > 1) {
       this.currentQuestion--;
-      this.progress = this.progress - 25;
-      console.log("Progress: " + this.progress);
+      this.countProgress();
     }
+  }
+
+  countProgress() {
+    let questionsCount = Object.keys(this.quizQuestions).length;
+    this.progress = (this.currentQuestion) / questionsCount * 100;
+    console.log("Progress: " + this.progress);
   }
 }
